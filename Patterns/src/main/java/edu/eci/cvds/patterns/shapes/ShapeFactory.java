a4a0d4fe720f486aa66d77baba6218803a4b1b36
unknown <juan.becerra-pe@labinfo.is.escuelaing.edu.co>
2022-02-01 08:55:07 -0500

    lab 2 terminado

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -0,0 +1,24 @@
+package edu.eci.cvds.patterns.shapes;
+import edu.eci.cvds.patterns.shapes.concrete.*;
+
+public class ShapeFactory{
+	public static Shape create(RegularShapeType figura){
+		switch (figura){
+			case Triangle:
+				return new Triangle();
+			
+			case Quadrilateral:
+				return new Quadrilateral();
+			
+			case Pentagon:
+				return new Pentagon();
+			
+			case Hexagon:
+				return new Hexagon();
+			
+			default:
+				return null;
+			
+		}
+	}
+}
